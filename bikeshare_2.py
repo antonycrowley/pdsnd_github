29dfe71f7c8030ef5881c4cf328923e8d4b021e1
Antonio Perez <perez.antonio87@gmail.com>
2023-01-31 21:20:50 -0600

    feat:changed city input for better user exp.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -25,7 +25,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington).
     # HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input('Please enter city name on which you want to review data: ').lower()
+        city = input('Please enter city name (Chicago, New York City, Washington) on which you want to review data: ').lower()
         if city in ['chicago', 'new york city', 'washington']:
             break
         else:
